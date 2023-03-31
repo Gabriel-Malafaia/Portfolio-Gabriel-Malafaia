2226037d41e0373e588531b55254081d1c866d9d
Gabriel Malafaia <gabrielbsnsp@gmail.com>
2023-03-31 10:15:23 -0300

    feat: change informations

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Gabriel Malafaia",
+  githubUser: "Gabriel-Malafaia",
+  linkedinUser: "gabrielmalafaia",
+  whatsappNumber: "+5521983751995",
+  emailUser: "gabrielbsnsp@gmail.com",
+  instagramUser: "gabrielmlf22",
   facebookUser: "",
 };
